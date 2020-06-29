bc092c50b6ce866eb7f3a15bf76a695e56054ca0
Victor Valdez <victormanuelvaldezblanco@gmail.com>
2020-06-29 16:07:40 -0400

    Initial commit, Entry point of credit calculator

---
+++ calculator.py
@@ -0,0 +1,40 @@
+
+
+# write your code here
+import math, argparse, sys
+
+    
+
+
+def error_parameters():
+    sys.exit('Incorrect parameters')
+
+
+#Program entry point
+parser = argparse.ArgumentParser(description="Credit Calculator")
+parser.add_argument('--t','--type', help="Describe the type of credit, Annuity or Diff (Differential)",
+                    choices=['annuity', 'diff'])
+parser.add_argument('--payment', help="The monthly pay ammount", type=int)
+parser.add_argument('--interest', help="Yearly interest rate", type=float)
+parser.add_argument('--periods', help="Number of months to repay", type=int)
+parser.add_argument('--principal', help="Credit ammount - Principal", type=int)
+
+args = parser.parse_args()
+
+if len(vars(args)) < 5:
+    error_parameters()
+
+if not args.t: 
+    error_parameters()
+if not args.interest:
+    error_parameters()
+
+if args.t == 'diff':
+    if args.payment:
+        error_parameters()
+    differentiate_payment()
+
+if args.t == 'annuity':
+    if not args.payment: annuity_payment()
+    if not args.principal: credit_principal_calculator()
+    if not args.periods: count_of_months()
